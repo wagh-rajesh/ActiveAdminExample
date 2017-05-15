be0a50b2ca948e91c9da2d6dd4c8ac0d69d817a3
wraj <waghrajesh6@gmail.com>
2017-05-16 01:22:43 +0530

    Configuring mailer for comments of posts.

---
+++ app/mailers/comment_mailer.rb
@@ -0,0 +1,13 @@
+class CommentMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.comment_mailer.new_comment.subject
+  #
+  def new_comment
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
